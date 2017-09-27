f45af282c48bb526c4d8e3d0d30be2c878fa6341
LATBauerdick <git@bauerdick.org>
2017-09-26 23:34:39 -0500

    v5.0.0 changes for pure11

---
+++ src/Data/Number/Format.cc
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Module      :  Format.cc
+// Copyright   :  (c) LATBauerdick 2017
+// License     :  MIT
+//
+// Maintainer  :  
+// Stability   :  experimental
+// Portability :
+//
+// Number_Format FFI functions
+//
+///////////////////////////////////////////////////////////////////////////////
+//
+#include "Format.hh"
+
+namespace Data_Number_Format {
+  using namespace PureScript;
+  std::string to_string(const any& f) {
+    std::string str = std::to_string (f);
+    int offset{1}; if (str.find_last_not_of('0') == str.find('.')) { offset = 0; }
+    str.erase(str.find_last_not_of('0') + offset, std::string::npos);
+    return str;
+  }
+}
+
