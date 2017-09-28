2c8aa39a0c081a37d5c787bf815e58bcbd753c25
LATBauerdick <git@bauerdick.org>
2017-09-28 13:47:31 -0500

    v5.0.0 changes for pure11

--- src/Data/Number/Format.cc
+++ src/Data/Number/Format.cc
@@ -19,8 +19,12 @@ namespace Data_Number_Format {
   using namespace PureScript;
   auto toString(const any& f) -> any {
     std::string str = std::to_string ((double)f);
-    int offset{1}; if (str.find_last_not_of('0') == str.find('.')) { offset = 0; }
-    str.erase(str.find_last_not_of('0') + offset, std::string::npos);
+    int offset{1};
+    int ln0 = str.find_last_not_of('0')
+    if (ln0 == str.find('.')) {
+      str.erase(ln0+2, std::string::npos);
+    } else
+      str.erase(ln0 + offset, std::string::npos);
     return str;
   }
 }
